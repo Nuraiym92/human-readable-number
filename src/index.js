2593e14016986321c1f8bf0467f3bcf71188c1e0
Nuraiym <nuraiym.zhyrgalbekova.kk@gmail.com>
2022-02-22 20:34:51 +0600

    fixed

--- src/index.js
+++ src/index.js
@@ -1,38 +1,12 @@
 module.exports = function toReadable (number) {
-    let numToWords = number => {
-        let a = [
-          '', 'one', 'two', 'three', 'four',
-          'five', 'six', 'seven', 'eight', 'nine',
-          'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
-          'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
-        ];
-        let b = [
-          '', '', 'twenty', 'thirty', 'forty',
-          'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
-        ];
-        let g = [
-          '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion',
-          'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion'
-        ];
-        // this part is really nasty still
-        // it might edit this again later to show how Monoids could fix this up
-        let makeGroup = ([ones,tens,huns]) => {
-          return [
-            num(huns) === 0 ? '' : a[huns] + ' hundred ',
-            num(ones) === 0 ? b[tens] : b[tens] && b[tens] + '-' || '',
-            a[tens+ones] || a[ones]
-          ].join('');
-        };
-        // "thousands" constructor; no real good names for this, i guess
-        let thousand = (group,i) => group === '' ? group : `${group} ${g[i]}`;
-        // execute !
-        if (typeof n === 'number') return numToWords(String(n));
-        if (n === '0')             return 'zero';
-        return comp (chunk(3)) (reverse) (arr(n))
-          .map(makeGroup)
-          .map(thousand)
-          .filter(comp(not)(isEmpty))
-          .reverse()
-          .join(' ');
-      };
+    var num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+    var tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+    
+    
+        if (n < 20) return num[number];
+        var digit = n%10;
+        if (n < 100) return tens[~~(n/10)-2] + (digit? "-" + num[digit]: "");
+        if (n < 1000) return num[~~(n/100)] +" hundred" + (n%100 == 0? "": " " + number2words(n%100));
+        return number2words(~~(n/1000)) + " thousand" + (n%1000 != 0? " " + number2words(n%1000): "");
+    
 }
